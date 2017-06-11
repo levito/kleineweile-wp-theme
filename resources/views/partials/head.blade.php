c1fc6a6140a2b236e8ccc9b5a8f77fed45919c87
Veit Lehmann <levito82@gmail.com>
2017-06-11 23:33:41 +0200

    clean-up styles, add basic rules + fonts

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,700,700i" rel="stylesheet">
   @php(wp_head())
 </head>
