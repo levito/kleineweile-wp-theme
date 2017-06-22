8988dc139b1ed9d0fef0d67a68b8e890edfd29c9
Veit Lehmann <levito82@gmail.com>
2017-06-23 00:14:15 +0200

    fix fonts, remove Lato

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://fonts.googleapis.com/css?family=Quattrocento:400,700|Lato:300,300i,700,700i|Amatic+SC:400" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Quattrocento:400,700|Amatic+SC:400" rel="stylesheet">
   @php(wp_head())
 </head>
