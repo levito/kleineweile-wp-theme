0b954343011f9d41e4c87fb98a2ca9b04e35fb69
Veit Lehmann <levito82@gmail.com>
2017-06-23 00:37:53 +0200

    remove unnused JS, add explicit widget-title class

--- app/setup.php
+++ app/setup.php
@@ -13,7 +13,7 @@ use Roots\Sage\Template\BladeProvider;
  */
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+    // wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 }, 100);
 
 /**
@@ -76,7 +76,7 @@ add_action('widgets_init', function () {
     $config = [
         'before_widget' => '<section class="widget %1$s %2$s">',
         'after_widget'  => '</section>',
-        'before_title'  => '<h3>',
+        'before_title'  => '<h3 class="widget-title">',
         'after_title'   => '</h3>'
     ];
     register_sidebar([
