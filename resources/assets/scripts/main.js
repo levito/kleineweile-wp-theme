966aeffc3b5e60bb2417530562fcf98453c47731
Veit Lehmann <levito82@gmail.com>
2017-06-24 01:14:25 +0200

    fix live reload: webpack needs an entry JS

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,24 +1,25 @@
-/** import external dependencies */
-import 'jquery';
+// /** import external dependencies */
+// import 'jquery';
 
-/** import local dependencies */
-import Router from './util/Router';
-import common from './routes/common';
-import home from './routes/home';
-import aboutUs from './routes/about';
+// /** import local dependencies */
+// import Router from './util/Router';
+// import common from './routes/common';
+// import home from './routes/home';
+// import aboutUs from './routes/about';
 
-/**
- * Populate Router instance with DOM routes
- * @type {Router} routes - An instance of our router
- */
-const routes = new Router({
-  /** All pages */
-  common,
-  /** Home page */
-  home,
-  /** About Us page, note the change from about-us to aboutUs. */
-  aboutUs,
-});
+// /**
+//  * Populate Router instance with DOM routes
+//  * @type {Router} routes - An instance of our router
+//  */
+// const routes = new Router({
+//   /** All pages */
+//   common,
+//   /** Home page */
+//   home,
+//   /** About Us page, note the change from about-us to aboutUs. */
+//   aboutUs,
+// });
 
-/** Load Events */
-jQuery(document).ready(() => routes.loadEvents());
+// /** Load Events */
+// jQuery(document).ready(() => routes.loadEvents());
+
