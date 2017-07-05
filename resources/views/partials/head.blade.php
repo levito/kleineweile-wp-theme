b1f073fe660c86743286eeec85398c0061195b93
Veit Lehmann <levito82@gmail.com>
2017-07-05 20:00:29 +0200

    improve content-editing styles for TinyMCE

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,5 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://fonts.googleapis.com/css?family=Gentium+Basic:400,400i,700,700i|Amatic+SC:400" rel="stylesheet">
   @php(wp_head())
 </head>
